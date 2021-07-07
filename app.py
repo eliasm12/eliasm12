2e564fb437c6f7306cb04f7e1541ffcca7efc88d
Elias Moussa <emoussa30@gmail.com>
2021-07-07 13:58:34 +0000

    updates

--- app.py
+++ app.py
@@ -17,8 +17,6 @@ app = App(
 slack_token = os.environ["SLACK_BOT_TOKEN"]
 client = WebClient(token=slack_token)
 
-
-
 #database connection
 mydb = mysql.connector.connect(
   host=os.environ.get("DBHOST"),
